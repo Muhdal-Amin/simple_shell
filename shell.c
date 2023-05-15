b449a196df8fcae99d1b6ffb9335ece2b78268d2
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-05-15 14:07:49 +0300

    Created shell.h, shell.c

---
+++ shell.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc - number of arguments
+ * @argv - array of strings of arguements
+ *
+ * Retrn: O on success, ...
+ */
+int main(int argc, char **argv)
+{
+	
+
+
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+
+
+
+
+
+
+#endif
