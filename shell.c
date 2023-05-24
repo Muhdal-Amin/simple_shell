350e66eb14e0392efed1bfc41d2f846072d9744f
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-05-24 20:54:18 +0300

    Problem git pushing

--- shell.c
+++ shell.c
@@ -22,91 +22,70 @@ int main(int argc, char **argv)
 	pid_t child_pid;
 
 	(void)argc;
-
 	while (1)
 	{
 		_puts(prompt);
-
 		read_count = getline(&lineptr, &n, stdin);
 		if (read_count == -1)
 		{
 			_puts("Error reading command\n");
 			return (-1);
 		}
+		if (cmpexit(lineptr, "exit") == 0)
+			exit(0);
+		if (cmpenv(lineptr, "env") == 0)
+		{
+			if (environ != NULL)
+			{
+				char **env = environ;
 
-		 if (cmpexit(lineptr, "exit") == 0)
-			 exit(0);
-
-		 if (cmpenv(lineptr, "env") == 0)
-		 {
-			 char **env = environ;
-			 while (*env != NULL)
-			 {
-				 if (!(_strcmp(*env, "USER")) ||
-				     !(_strcmp(*env, "LANGUAGE")) ||
-				     !(_strcmp(*env, "SESSION")) ||
-				     !(_strcmp(*env, "COMPIZ_CONFIG_PROFILE")) ||
-				     !(_strcmp(*env, "SHLV")) ||
-				     !(_strcmp(*env, "HOME")) ||
-				     !(_strcmp(*env, "C_IS")) ||
-				     !(_strcmp(*env, "DESKTOP_SESSION")) ||
-				     !(_strcmp(*env, "LOGNAME")) ||
-				     !(_strcmp(*env, "TERM")) ||
-				     !(_strcmp(*env, "PATH")))
-				 {
-					 _puts(*env);
-					 _puts("\n");
-				 }
-				 env++;
-			 }
-		 }
-		 
-		 lineptr_dup = malloc(sizeof(char) * read_count);
-		 if (!lineptr_dup)
-		 {
-			 perror("Memory Allocation Error");
-			 return (-1);
-		 }
-		 _strcpy(lineptr_dup, lineptr);
-		
-		 tok_count = 0;
-		 token = strtok(lineptr, delim);
-		 while (token)
-		 {
-			 tok_count++;
-			 token = strtok(NULL, delim);
-		 }
-		 tok_count++;
-		 
-		 argv = malloc(sizeof(char *) * tok_count);
-		 token = strtok(lineptr_dup, delim);
-		 for (i = 0; token != NULL; i++)
-		 {
-			 argv[i] = malloc(sizeof(char) * _strlen(token));
-			 _strcpy(argv[i], token);
-
-			 token = strtok(NULL, delim);
-		 }
-		 argv[i] = NULL;
-		 
-		 child_pid = fork();
-		 if (child_pid == -1)
-		 {
-			 perror("Forking Error");
-			 return (-1);
-		 }
-		 if (child_pid == 0)
-		 {
-			 execve_cmd(argv);
-		 }
-		 else
-			 wait(&status);
+				while (*env != NULL)
+				{
+					_puts(*env);
+					_puts("\n");
+					env++;
+				}
+			}
+		}
+		lineptr_dup = malloc(sizeof(char) * read_count);
+		if (!lineptr_dup)
+		{
+			perror("Memory Allocation Error");
+			return (-1);
+		}
+		_strcpy(lineptr_dup, lineptr);
+		tok_count = 0;
+		token = strtok(lineptr, delim);
+		while (token)
+		{
+			tok_count++;
+			token = strtok(NULL, delim);
+		}
+		tok_count++;
+		argv = malloc(sizeof(char *) * tok_count);
+		token = strtok(lineptr_dup, delim);
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * _strlen(token));
+			_strcpy(argv[i], token);
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Forking Error");
+			return (-1);
+		}
+		if (child_pid == 0)
+		{
+			execve_cmd(argv);
+		}
+		else
+			wait(&status);
 	}
-
 	free(lineptr);
 	free(lineptr_dup);
 	free(argv);
-
 	return (0);
-
 }
