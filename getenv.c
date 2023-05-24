9a131e6a05f89db2dbb05583f1c996d25c38ed01
AL 'AMIN <113265168+Muhdal-Amin@users.noreply.github.com>
2023-05-24 18:47:42 +0100

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -9,26 +9,23 @@
 
 char *_getenv(const char *name)
 {
-<<<<<<< HEAD
 	size_t name_length = strlen(name);
-=======
-        extern char **environ;
+	
+	extern char **environ;
         size_t name_length = strlen(name);
 	int i = 0;
->>>>>>> 61fe9e69701827ebbb0287651650a779514f75a1
 
 	if (name_length == 0 || environ == NULL)
 		return (NULL);
 
-<<<<<<< HEAD
 	for (int i = 0; environ[i] != NULL; i++)
 	{
 		if (_strncmp(name, environ[i], name_length) == 0 &&
 		    environ[i][name_length] == '=')
 			return (&environ[i][name_length + 1]);
        }
-=======
-        for (i = 0; environ[i] != NULL; i++)
+
+	for (i = 0; environ[i] != NULL; i++)
         {
                 if (_strncmp(name, environ[i], name_length) == 0 && environ[i][name_length] == '=')
                         return &environ[i][name_length + 1];
